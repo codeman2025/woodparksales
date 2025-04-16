0f61b1d6459548ff124ca1b8b9bb396f6c60b4de
Lucas <145366816+codeman2025@users.noreply.github.com>
2025-04-16 22:00:01 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,60 @@
+// Name of the cache
+const CACHE_NAME = 'woodpark-admin-cache-v1';
+
+// Files to cache
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/logo.png',
+  '/images/icon-512x512.png',
+  '/styles.css',
+];
+
+// Install the service worker and cache necessary files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activate the service worker
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            // Delete outdated cache
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch data and serve from cache first, falling back to the network if needed
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse; // Serve from cache
+      }
+
+      return fetch(event.request).then((response) => {
+        // Cache the response for future requests
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
